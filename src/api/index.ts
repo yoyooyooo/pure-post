bd56750bb465114763a1a473a6d8ad00b6258269
yoyo <jihu777@gmail.com>
2020-12-31 14:03:52 +0800

    debug

--- src/api/index.ts
+++ src/api/index.ts
@@ -12,6 +12,7 @@ export default async (req: NowRequest, res: NowResponse) => {
   const { url, pure, ...rest } = req.query as any;
   const matchKey = [...urlMap.keys()].find((reg) => reg.test(url));
   console.log('matchKey', matchKey, urlMap.get(matchKey));
+  console.log('pure', pure);
   if (matchKey) {
     const r = await axios(
       `https://${process.env.PREFIX_URL}.vercel.app/api/${urlMap.get(
