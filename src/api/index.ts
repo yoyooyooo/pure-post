206f6d5bbb8ffe7f65852f6326abc9fc8434eaa4
Jackie <jihu777@gmail.com>
2021-11-21 23:01:37 +0800

    fix: decode
    fix: decode

--- src/api/index.ts
+++ src/api/index.ts
@@ -5,13 +5,13 @@ import urlMap from "../utils/urlMap";
 
 export default async (req: VercelRequest, res: VercelResponse) => {
   const query = req.query as any;
-  const matchKey = [...urlMap.keys()].find((reg) => reg.test(query.url));
+  const matchKey = [...urlMap.keys()].find((reg) => reg.test(decodeURIComponent(query.url)));
   console.log("matchKey", matchKey, urlMap.get(matchKey));
 
   if (matchKey) {
     const _url = `https://${process.env.PREFIX_URL}.vercel.app/api/${urlMap.get(
       matchKey
-    )}?${qs.stringify(query)}`;
+    )}?${decodeURIComponent(qs.stringify(query))}`;
     console.log("match url", _url);
     const r = await axios(_url);
     res.send(r.data);
