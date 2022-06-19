268d5a4224ef9698efa2896d6d0690f3a3ea7538
Jackie <jihu777@gmail.com>
2022-06-19 08:52:15 +0800

    fix: urlMap

--- src/utils/urlMap.ts
+++ src/utils/urlMap.ts
@@ -1,6 +1,7 @@
 export default new Map([
   [/zhuanlan\.zhihu\.com\/p\/.*/, "zhihu-answer"],
   [/zhihu\.com\/question\/\d+\/answer\/.*/, "zhihu-answer"],
+  [/zhihu\.com\/answer\/.*/, "zhihu-answer"],
   [/mp\.weixin\.qq\.com/, "weixin"],
   [/daily\.zhihu\.com/, "daily"]
 ]);
